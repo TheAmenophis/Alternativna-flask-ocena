40da966af930e24644aed16d85f47608d411faeb
Amenophis <tonklialjaz@gmail.com>
2026-05-26 08:46:10 +0000

    prva vaja

--- app1.py
+++ app1.py
@@ -10,12 +10,55 @@ app.secret_key = "superduperskrivnost"
 
 db = TinyDB('db.json')
 users = db.table('users')
+notes = db.table('notes')
 
 User = Query()
+Notes = Query()
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def home():
+    if "user" in session:
+        return redirect("/dashboard")
+    
+    return redirect("/login")
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user = users.get(User.username == username)
+        if user and user["password"] == password:
+            session["user"] = username
+            return redirect("/dashboard")
+        return "Napaka pri loginu"
+    
+    return render_template("login.html")
+
+@app.route("/register", methods=["GET","POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if users.search(User.username == username):
+            return "Uporabnik obstaja"
+        
+        users.insert({"username" : username, "password": password, "note" : ""})
+        return redirect("/login")
+    
+    return render_template("register.html")
+
+@app.route("/dashboard")
+def dashboard():
+    if "user" not in session:
+        return redirect("/login")
+    
+
+    userNotes = notes.search(Notes.username == session["user"])
+
+    return render_template("dashboard.html", user=session["user"], notes=userNotes)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
