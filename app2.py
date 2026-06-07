47ba23309df17e60b3d2d8da34f0a87e5b03d8df
Amenophis <tonklialjaz@gmail.com>
2026-06-07 11:30:02 +0000

    vaja 2

--- app2.py
+++ app2.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, session, jsonify
 from tinydb import TinyDB, Query
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(
     __name__,
@@ -8,8 +10,12 @@ app = Flask(
 )
 app.secret_key = "superduperskrivnost2"
 
+UPLOAD_FOLDER = "static2/uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
 db = TinyDB('db2.json')
 users = db.table('users')
+posts = db.table("posts")
 
 User = Query()
 
@@ -43,7 +49,7 @@ def register():
         if users.search(User.username == username):
             return "Uporabnik obstaja"
         
-        users.insert({"username" : username, "password": password, "note" : ""})
+        users.insert({"username" : username, "password": password})
         return redirect("/login")
     
     return render_template("register.html")
@@ -53,7 +59,57 @@ def index():
     if "user" not in session:
         return redirect("/login")
     
-    return render_template("index.html", user=session['user'])
+    vse_objave = posts.all()
+    vse_objave.reverse()
+
+    return render_template("index.html", user=session['user'], posts=vse_objave)
+
+@app.route("/add_post", methods=["POST"])
+def add_post():
+    if "user" not in session:
+        return redirect("/login")
+    
+    text = request.form["text"]
+    image = request.files["image"]
+    image_path = ""
+
+    if image and image.filename != "":
+        filename = secure_filename(image.filename)
+        image.save(os.path.join(UPLOAD_FOLDER, filename))
+        image_path = "uploads/" + filename 
+
+    posts.insert({
+        "username": session["user"],
+        "text": text,
+        "image": image_path
+    })
+
+    return redirect("/index")
+
+@app.route("/logout", methods=["GET"])
+def logout():
+    session.clear()
+    return redirect("/login")
+
+@app.route("/delete_post/<int:doc_id>")
+def delete_post(doc_id):
+    if "user" not in session:
+        return redirect("/login")
+
+    post = posts.get(doc_id=doc_id)
+
+    if post is None:
+        return redirect("/index")
+
+    if post["username"] == session["user"]:
+        posts.remove(doc_ids=[doc_id])
+        if post["image"]:
+            path = os.path.join("static2", post["image"])
+
+            if os.path.exists(path):
+                os.remove(path)
+
+    return redirect("/index")
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
