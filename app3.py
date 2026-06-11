79d162cd4cdf30197b907ee25b54cd84d33a8585
Amenophis <tonklialjaz@gmail.com>
2026-06-11 13:25:04 +0000

    app3,html za tretjo

--- app3.py
+++ app3.py
@@ -1,14 +1,51 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from tinydb import TinyDB, Query
 
 app = Flask(
     __name__,
     template_folder="templates3",
     static_folder="static3"
 )
+app.secret_key = "superduperskrivnost"
+
+db = TinyDB('db.json')
+users = db.table('users')
+messages = db.table('messages')
+
+User = Query()
+Message = Query()
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user = users.get(User.username == username)
+        if user and user["password"] == password:
+            session["user"] = username
+            return redirect("/dashboard")
+        return "Napaka pri loginu"
+    
+    return render_template("login.html")
+
+@app.route("/register", methods=["GET","POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if users.search(User.username == username):
+            return "Uporabnik obstaja"
+        
+        users.insert({"username" : username, "password": password, "note" : ""})
+        return redirect("/login")
+    
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
